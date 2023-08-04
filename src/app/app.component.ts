d52ac7fb547a966eafd699f09102358c7ace4df1
rahmabacha <rahmabacha007@gmail.com>
2023-08-04 01:11:58 +0100

    still testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    events: [
+      // Load your events here from your backend or data source
+    ],
+  };
   title = 'rendez-vous';
+
+  constructor() {}
+
+
+
+  ngOnInit(): void {
+    this.calendarOptions = {
+      // ... other options
+      eventClick: this.handleEventClick.bind(this),
+      selectable: true,
+      select: this.handleDateSelect.bind(this),
+    };
+  }
+  
+  handleEventClick(eventInfo: { event: any; }) {
+    // Handle event click logic
+    console.log('Event clicked:', eventInfo.event);
+  }
+  
+  handleDateSelect(selectInfo: { startStr: any; endStr: any; }) {
+    // Handle date selection logic
+    console.log('Date selected:', selectInfo.startStr, selectInfo.endStr);
+  }
+  
 }
